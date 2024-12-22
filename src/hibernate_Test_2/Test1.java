4a2e0dee18240b95b7f670abf4c4233cc1a7b89a
Gorg <Spring_course@gmail.com>
2024-12-22 16:36:33 +0100

    Add commit 2

--- src/hibernate_Test_2/Test1.java
+++ src/hibernate_Test_2/Test1.java
@@ -12,7 +12,7 @@ public class Test1 {
         SessionFactory factory = new Configuration().configure("Hibernate.cfg.xml").
                 addAnnotatedClass(Employee.class).addAnnotatedClass(Detail.class).buildSessionFactory();
         try {
-            Session session = factory.getCurrentSession();
+/*            Session session = factory.getCurrentSession();
             Employee employee = new Employee ("Graf Monte","Kristo","Shuris madiebeli",520202550);
             Detail detail = new Detail ("Berlin","+49176809090","Graf_Monte_Kristo@gmail.com");
 
@@ -21,7 +21,15 @@ public class Test1 {
 
             session.save(employee);
             session.getTransaction().commit();
+            System.out.println(" ##--  Done!!!   -## ");*/
+            Session session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class, 5);
+            System.out.println(emp.getEmpDetail());
+            session.getTransaction().commit();
             System.out.println(" ##--  Done!!!   -## ");
+
 
         }
         finally {
